b50074d7582a4e7f38b60b7f0e871fd60952bfb0
Joe Jackson <jbudorg@gmail.com>
2020-03-13 20:36:50 -0700

    cleanup shop
    features:
    - variable canvas size
    - variable array size
    - original bogo algorithm added
    - ability to reset
    - user interactivity
    
    removed:
    - async functions and calls, we will just set the framerate if we want to slow it down.

---
+++ algorithms.js
@@ -0,0 +1,62 @@
+
+/**
+  * These algorithms are in-line and called inside a loop.
+  * -they rely on global variables set in sketch.js
+  */
+
+function bogoSort(){
+  if (!isSorted(arr)) {
+    arrayshuffle(arr, 0, arr.length);
+    arrayshuffles++;
+  } else {
+    sorted = true;
+    noLoop(); // Stop the loop, bogo actually worked?
+  }
+}
+
+
+function betterBogoSort(){
+  if (pos < end - 1) {
+    tmp = arr[pos];
+    for (x = pos + 1; x < end; x++) {
+      if (arr[x] < tmp) {
+        break; // Break out of this for loop without iterating x.
+      }
+    }
+    if (x == end) {
+      pos++; // this element is in the right place!!
+    } else {
+      arrayshuffle(arr, pos, end);
+      arrayshuffles++;
+    }
+  } else {
+    noLoop(); // array is sorted!
+  }
+}
+
+// Helpers:
+
+function arrayshuffle(arr, start, end) {
+  let r;
+  let size = end - start;
+  for (let i = start; i < end; i++) {
+    r = Math.floor(Math.random() * size);
+    swap(arr, i, start + r);
+  }
+}
+
+function swap(arr, z, y) {
+  let t = arr[z];
+  arr[z] = arr[y];
+  arr[y] = t;
+  arrayaccesses += 2;
+}
+
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
--- sketch.js
+++ sketch.js
@@ -1,73 +1,136 @@
+let wi = 400; // Canvas Width (example: 800)
+let hi = 300; // Canvas Height (example: 600)
+let arrsize = 100; // Array Length (Limit: Width / 2 or it breaks!)
+let fr = 30; // Framerate (example: 5)
+
+let algs = {
+  bogo: 'Bogo',
+  betterBogo: 'Better Bogo'
+}
+
+// Global Variables:
+//*****
+let arr = [];
+//*****
 
 let arrayshuffles = 0;
 let arrayaccesses = 0;
-let arr = [];
-let pos,success,tmp,
-    x,start,end,oAccesses, oShuffles;
-let wi = 800;
-let hi = 600;
-async function arrayshuffle(arr,start, end) {
-	let r;
-	let size = end - start;
-	for (let i = start; i < end; i++) {
-		r = Math.floor(Math.random() * size);
-		swap(arr, i, start + r);
-	}
-}
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-async function swap(arr,z, y){
-  sleep(6);
-	let t = arr[z];
-	arr[z] = arr[y];
-	arr[y] = t;
-  arrayaccesses += 2;
+let sorted = false;
+let pos = 0;
+
+let tmp, x, end,
+  oAccesses, slider, bStart,
+  lAlg, iCount;
+
+
+function begin() {
+  bStart.attribute('disabled',true); // turn off the start button.
+  loop();
 }
 
 function setup() {
+
+  frameRate(fr);
   createCanvas(wi, hi);
-  for(i=0;i<100;i++){
-		arr.push(i);
-	}
-	arrayshuffle(arr,0,arr.length);
-  //betterBogo(arr,0,arr.length-1);
-  start = 0;
+  noLoop(); // do not start drawing by default (draw() will still be called
+            // once in some extremely rare cases this will cause a crash due
+            // to an array being shuffled with sorted vales.)
+
+  // populate the array with non-repeating iterated values, and shuffle
+  for (i = 0; i < arrsize; i++) {
+    arr.push(i);
+  }
+  arrayshuffle(arr, 0, arr.length);
+
   end = arr.length;
-  pos = start;
+  pos = 0;
+
+  // setup some interactivity:
+  lAlg = createSelect();
+  lAlg.option(algs.betterBogo);
+  lAlg.option(algs.bogo);
+  bStart = createButton('Sort!');
+  lAlg.changed(reset);
+  bStart.mousePressed(begin);
+  iCount = createInput(arrsize.toString());
+  iCount.input(function(){
+    arrsize = this.value();
+    reset();
+  });
+
+  // output:
   oAccesses = createP('');
-  oShuffles = createP('');
+
 }
+
+function reset(){
+
+  pos = 0;
+  loop(); // animate color changes to default. (in rare cases, this can cause a
+          // crash due to a random array being sorted upon creation) to prevent
+          // this, move noLoop() (from a few lines down) here and
+          //comment this line out.
+
+  // erase the array and repopulate it
+  arr = [];
+  for (i = 0; i < arrsize; i++) {
+    arr.push(i);
+  }
+  arrayshuffle(arr, 0, arr.length);
+
+  sorted = false; // reset sorted after new array is shuffled.
+
+  end = arr.length;
+
+  noLoop(); // stop the loop.
+  bStart.removeAttribute('disabled'); // turn on the start button.
+}
+
+
 function draw() {
-  background(0);
-  if (pos < end-1) {
-		tmp = arr[pos];
-		for (x = pos + 1; x < end; x++) {
-			if (arr[x] < tmp) {
-				break;
-			}
-		}
-		if (x == end) {
-			pos++;
-		} else {
-			arrayshuffle(arr, pos, end);
-			arrayshuffles++;
-		}
-	} else {
-    noLoop();
+
+  // Algorithm loop stuff:
+  switch(lAlg.value()){
+    case algs.bogo:
+      bogoSort();
+      break;
+    case algs.betterBogo:
+      betterBogoSort();
+      break;
+    default:
+      break;
   }
 
 
-  oAccesses.html("Array Accesses: "+arrayaccesses);
-  oShuffles.html("Array Shuffles: "+arrayshuffles);
+  // Draw stuff:
+  background(0);
+  oAccesses.html("Array Accesses: " + arrayaccesses +
+    " Array Shuffles: " + arrayshuffles
+  );
   for (let i = 0; i < arr.length; i++) {
-    stroke(255);
-    if (i < pos){
-      stroke(0,255,0);
-    } else if (i==pos) {
-      stroke(255,0,0);
+    switch(lAlg.value()){
+      case algs.bogo:
+        stroke(255,0,0);
+        if (sorted){
+          stroke(0,255,0);
+        }
+        break;
+      case algs.betterBogo:
+        stroke(255);
+        if (i < pos) {
+          stroke(0, 255, 0);
+        } else if (i == pos) {
+          stroke(255, 0, 0);
+        }
+        break;
+      default:
+        break;
     }
-    strokeWeight(7);
-    line(i*(wi/100)+4, height, i*(wi/100)+4, hi - arr[i]*6);
+    strokeWeight(Math.floor(wi / arrsize) - 1);
+    let linewidth = i * Math.floor(wi / arrsize);
+    let offsetw = Math.floor((wi / arrsize) / 2);
+    let offseth = Math.floor(hi / arrsize);
+    line(linewidth + offsetw, height, linewidth + offsetw, hi - arr[i] * offseth);
+    if (sorted) noLoop();
   }
 }
